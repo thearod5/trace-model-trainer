51e73253124c6bfeef86cf061ce53c2a5e00d030
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-06-08 18:12:34 -0500

    attempting to put into function to see if gb will recover memory

--- src/search.py
+++ src/search.py
@@ -1,6 +1,6 @@
 import gc
 from itertools import product
-from typing import List
+from typing import Dict, List
 
 import torch
 from pandas import DataFrame
@@ -10,6 +10,7 @@ from constants import OUTPUT_PATH
 from infra.eval import eval_model
 from infra.generic_trainer import generic_train
 from tdata.reader import read_project
+from tdata.trace_dataset import TraceDataset
 from utils import get_gpu_memory_usage
 
 
@@ -24,31 +25,38 @@ def search(train_dataset_path: str, test_dataset_path: str, models: List[str], o
         entries.append(metrics)
 
         for iterable_kwargs in tqdm(options, desc="Iterating through options"):
-            if torch.cuda.is_available():
-                print(f"GPU Memory Usage before training {model_name}: {get_gpu_memory_usage()}")
-
-            # Train
-            trained_model = generic_train(train_dataset,
-                                          model_name=model_name,
-                                          output_path=OUTPUT_PATH,
-                                          disable_tqdm=disable_logs,
-                                          **kwargs,
-                                          **iterable_kwargs)
-
-            metrics, predictions = eval_model(trained_model, test_dataset, disable_logs=disable_logs)
-            entries.append({"model": model_name, **iterable_kwargs, **metrics})
-
-            # cleanup
-            trained_model.to('cpu')
-            del trained_model
-            gc.collect()
-            torch.cuda.empty_cache()
-
-            # Display GPU memory usage after cleanup
-            if torch.cuda.is_available():
-                print(f"GPU Memory Usage after cleaning up {model_name}: {get_gpu_memory_usage()}")
+            entry = run_iteration(train_dataset, test_dataset, model_name, disable_logs=disable_logs, iterable_kwargs=iterable_kwargs,
+                                  **kwargs)
+            entries.append(entry)
 
     return DataFrame(entries)
+
+
+def run_iteration(train_dataset: TraceDataset, test_dataset: TraceDataset, model_name: str, disable_logs: bool, iterable_kwargs: Dict,
+                  **kwargs):
+    if torch.cuda.is_available():
+        print(f"GPU Memory Usage before training {model_name}: {get_gpu_memory_usage()}")
+
+    # Train
+    trained_model = generic_train(train_dataset,
+                                  model_name=model_name,
+                                  output_path=OUTPUT_PATH,
+                                  disable_tqdm=disable_logs,
+                                  **iterable_kwargs,
+                                  **kwargs)
+
+    metrics, predictions = eval_model(trained_model, test_dataset, disable_logs=disable_logs)
+
+    # cleanup
+    trained_model.to('cpu')
+    del trained_model
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # Display GPU memory usage after cleanup
+    if torch.cuda.is_available():
+        print(f"GPU Memory Usage after cleaning up {model_name}: {get_gpu_memory_usage()}")
+    return {"model": model_name, **iterable_kwargs, **metrics}
 
 
 def expand_dict_combinations(input_dict):
